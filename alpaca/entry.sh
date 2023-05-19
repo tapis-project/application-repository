921b126fd4fa1164e4c1e6c9df06aa248e4c236e
Joe Stubbs <joestubbs5@gmail.com>
2023-05-19 18:23:54 -0500

    Parameterize entry for Tapis app and provide initial app def.

--- alpaca/entry.sh
+++ alpaca/entry.sh
@@ -2,24 +2,26 @@
 
 cd /app
 
-torchrun --nproc_per_node=4 --master_port=54321 train.py \
-    --model_name_or_path "facebook/opt-6.7b" \
+echo "Setting HUGGINGFACE_HUB_CACHE to $1..."
+export HUGGINGFACE_HUB_CACHE=$1
+
+torchrun --nproc_per_node=$2 --master_port=54321 train.py \
+    --model_name_or_path "$3" \
     --data_path ./alpaca_data.json \
     --fp16 True \
     --output_dir /TapisOutput \
-    --num_train_epochs 3 \
-    --per_device_train_batch_size 4 \
-    --per_device_eval_batch_size 4 \
-    --gradient_accumulation_steps 8 \
-    --evaluation_strategy "no" \
-    --save_strategy "steps" \
-    --save_steps 2000 \
-    --save_total_limit 1 \
-    --learning_rate 2e-5 \
-    --weight_decay 0. \
-    --warmup_ratio 0.03 \
-    --lr_scheduler_type "cosine" \
-    --logging_steps 1 \
-    --fsdp "full_shard auto_wrap" \
+    --num_train_epochs $4 \
+    --per_device_train_batch_size $5 \
+    --per_device_eval_batch_size $6 \
+    --gradient_accumulation_steps $7 \
+    --evaluation_strategy "$8" \
+    --save_strategy "$9" \
+    --save_steps ${10} \
+    --save_total_limit ${11} \
+    --learning_rate ${12} \
+    --weight_decay ${13} \
+    --warmup_ratio ${14} \
+    --lr_scheduler_type "${15}" \
+    --logging_steps ${16} \
+    --fsdp "${17}" \
     --fsdp_transformer_layer_cls_to_wrap 'OPTDecoderLayer' \
-    --tf32 True
