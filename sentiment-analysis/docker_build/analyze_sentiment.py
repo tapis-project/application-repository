d3a15b5692e58379074fb64887a45d9010a88c21
Ubuntu <ubuntu@jo25672-vm.js2local>
2023-06-09 16:14:45 +0000

    made small changes to the app code

--- sentiment-analysis/docker_build/analyze_sentiment.py
+++ sentiment-analysis/docker_build/analyze_sentiment.py
@@ -2,13 +2,20 @@ import argparse
 
 from transformers import pipeline
 
+FLAGS = None
+
 def analyze():
     """
     Function that analyzes the sentence that the user provided.
+
+    Returns:
+        results (List): sentiment analysis results.
     """
     returnAllScores = FLAGS.return_all_scores != 'False'
-    classifier = pipeline("sentiment-analysis", model="j-hartmann/emotion-english-distilroberta-base", return_all_scores=returnAllScores)
-    print_results(classifier(FLAGS.sentence))
+    analyzer = pipeline("sentiment-analysis", model="j-hartmann/emotion-english-distilroberta-base", return_all_scores=returnAllScores)
+    results = analyzer(FLAGS.sentence)
+
+    return results
 
 def print_results(results):
     """
@@ -31,9 +38,13 @@ def print_results(results):
                 score = res['score']
                 print(f'{label}: {score:.2%}')
 
+def main():
+    results = analyze()
+    print_results(results)
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--sentence', type=str, default='', help='Sentence for analyzing.')
     parser.add_argument('--return_all_scores', type=str, default='True', help='Option to return all scores.')
     FLAGS, unparsed = parser.parse_known_args()
-    analyze()
+    main()
