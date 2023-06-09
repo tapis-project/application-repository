34706d1593e06b772badc0c7e1a1375f0116d87a
Ubuntu <ubuntu@jo25672-vm.js2local>
2023-06-09 20:58:40 +0000

    finzalized analyze_sentiment.py

--- sentiment-analysis/docker_build/analyze_sentiment.py
+++ sentiment-analysis/docker_build/analyze_sentiment.py
@@ -1,34 +1,47 @@
 import argparse
-
 from transformers import pipeline
 
 FLAGS = None
 
-def analyze():
+def analyze() -> list:
     """
     Function that analyzes the sentence that the user provided.
+
+    Args:
+        FLAGS.sentence (str): The sentence to be analyzed.  
+        FLAGS.return_all_scores (str): Option to return all scores or just the highest.
+        FLAGS.model (str): A custom model from the Hugging Face hub.
+            - optional model: j-hartmann/emotion-english-distilroberta-base
 
     Returns:
         results (List): sentiment analysis results.
     """
-    returnAllScores = FLAGS.return_all_scores != 'False'
-    analyzer = pipeline("sentiment-analysis", model="j-hartmann/emotion-english-distilroberta-base", return_all_scores=returnAllScores)
+    #the returnAllScores will always default to True, unless False is given
+    returnAllScores = FLAGS.return_all_scores.lower() not in ('false', 'f')
+
+    #if the model is empty, it uses a basic model, else uses provided model
+    if not FLAGS.model:
+        analyzer = pipeline("sentiment-analysis", return_all_scores=returnAllScores)
+    else:
+        analyzer = pipeline("sentiment-analysis", model=FLAGS.model, return_all_scores=returnAllScores)
+    
     results = analyzer(FLAGS.sentence)
 
     return results
 
-def print_results(results):
+def print_results(results: list):
     """
     Function that prints the results.
 
     Args:
         results (List): The results of the analysis.
     """
+    #if there was no sentence provided, prints this message, else prints the results
     if FLAGS.sentence == '':
         print("There was no sentence to analyze.")
     else:
         print("The sentence: " + FLAGS.sentence)
-        if FLAGS.return_all_scores == 'False':
+        if FLAGS.return_all_scores.lower() in ('false', 'f'):
             label = results[0]['label']
             score = results[0]['score']
             print(f'{label}: {score:.2%}')
@@ -44,7 +57,11 @@ def main():
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
+
     parser.add_argument('--sentence', type=str, default='', help='Sentence for analyzing.')
     parser.add_argument('--return_all_scores', type=str, default='True', help='Option to return all scores.')
+    parser.add_argument('--model', type=str, default='', help='Optional model name')
+
     FLAGS, unparsed = parser.parse_known_args()
+
     main()
