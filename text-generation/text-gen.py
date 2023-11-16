f69f3c086666a62ef9f390c4462873f7cd950cd6
tacc-afields <afields@tacc.utexas.edu>
2023-11-16 15:08:14 -0600

    added the ability to add urls. Fixed main() and write to file(). Cleaning up the generated output.

--- text-generation/text-gen.py
+++ text-generation/text-gen.py
@@ -1,29 +1,34 @@
 import argparse, os, requests, sys
 
 from bs4 import BeautifulSoup
-
 from transformers import pipeline
 
 
 def url_to_file(flags):
     #Get content
     content = requests.get(flags.url).text
-    prettified_content = BeautifulSoup(content, 'html.parser').prettify()
     soup = BeautifulSoup(content, 'html.parser')
 
+    
+    #remove footer
+    content_footer = soup.find('footer')
+    if content_footer:
+        content_footer.decompose()
+    
+    content = soup.get_text()
+    content.replace("\n", "")
+    
     #write content to file
     with open(f"./text-generator-files/url_as_file.txt", "w", encoding="utf-8") as file:
-        file.write(prettified_content)
+        file.write(str(content))
 
-    print(soup.title.string)
-"""
 def gen_text(flags):
     
     '''
     Function to generate text from input sentence, using the TextGenerationPipeline from huggingface. 
     Args:
         model_name (str): Text generation model name, please see: https://huggingface.co/models?pipeline_tag=text-generation&sort=downloads
-        sentence (str): The input sentence for text generation.
+        statement (str): The input sentence for text generation.
         max_num (int): Input to control the total length of the output text are generated.
         num_seq (int): Input to control how many different sequences are generated.
     Output:
@@ -36,26 +41,33 @@ def gen_text(flags):
     sequence = flags.sequence
     max_output = flags.max_output
     file = flags.file
+    url = flags.url
+    url_file_used = "./text-generator-files/url_as_file.txt"
+
+    if statement == None and file == None and url == None:
+        raise Exception("You must provide either a statement, a file, or a URL")
     
-    try:
-        max_len = int(max_len)
-        num_seq = int(num_seq)
-    except Exception as e:
-        return f'Error: {e}. Please input integer for max_len and num_seq'
+    if file and not os.path.isfile(file):
+        raise Exception(f"A path provided in the --file flag is not a file | {file}")
+
+
+    if file == None:
+        with open(url_file_used, mode="r", encoding="utf-8") as file_obj:
+            statement = file_obj.read()
+    else:
+        with open(file, mode="r", encoding="utf-8") as file_obj:
+            statement = file_obj.read()
 
     try:
-        generator = pipeline("text-generation", model=model_name) # tested using "distilgpt2"
-    except Exception as e:
-        return f'Error: {e}. Please check if model inputted is compatible.'
-
-    try:
-        output_text = generator(sentence, max_length=max_len, num_return_sequences=num_seq,)
+        output = pipeline("text-generation")
+        generated_text = output(statement, model=model, max_length=max_output, num_return_sequences=sequence)
     except Exception as e:
         return e
 
-    return (output_text)
+    return(str(generated_text[0]['generated_text'].replace("'\n'", "").encode("utf-8")))
 
-def write_to_file(results: str, filename: str):
+
+def write_to_file(generated_text):
     '''
     Function writes results string into a file.
     Args:
@@ -65,21 +77,27 @@ def write_to_file(results: str, filename: str):
         None
     '''
         # output file destination
-    directory = "text-generator"
-    filename = os.path.join(directory, "output.txt")
+    output_filename = os.path.join(
+        os.environ.get(
+            "_tapisExecSystemOutputDir",
+            "text-generator-files"
+        ),
+        "output.txt"
+    )
+    
     
     try:
-        os.makedirs(os.path.dirname(filename), exist_ok=True)
-        with open(filename, 'w') as file:
-            file.write(results)
-        print("Output written to", filename)
+        os.makedirs(os.path.dirname(output_filename), exist_ok=True)
+        with open(output_filename, 'w') as file:
+            file.write(generated_text)
+        print("Output written to", output_filename)
     except IOError:
-        print("Error writing to file", filename)
+        print("Error writing to file", output_filename)
         
     #with open(filename, 'r') as file:
     #print(file.read())
+    
 
-"""
 def main():
     
     
@@ -97,6 +115,7 @@ def main():
 
     #Run the program
     url_to_file(flags)
+    print(write_to_file(gen_text(flags)))
 
 if __name__ == "__main__":
     main()
